6df6f36e02cd910ad7accc90660fc891f6c646a1
Elijah <elijahsharper1@gmail.com>
2018-02-05 02:34:26 +0300

    ReflectionClass and renamed the event

---
+++ tests/Stubs/UserSignedUp.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Tests\Stubs;
+
+use App\Core\Events\Event;
+
+class UserSignedUp extends Event
+{
+
+}
--- tests/Unit/EventTest.php
+++ tests/Unit/EventTest.php
@@ -3,15 +3,23 @@
 namespace App\Tests\Unit;
 
 use PHPUnit\Framework\TestCase;
-use App\Tests\Stubs\EventStub;
+use App\Tests\Stubs\{UserSignedUp, EventStubNoName};
 
 class EventTest extends TestCase
 {
   /** @test */
   public function can_get_event_name()
   {
-    $event = new EventStub();
+    $event = new UserSignedUp();
 
-    $this->assertEquals('EventStub', $event->getName());
+    $this->assertEquals('UserSignedUp', $event->getName());
+  }
+
+  /** @test */
+  public function it_defaults_to_an_event_name_of_the_class_name()
+  {
+    $event = new EventStubNoName();
+
+    $this->assertEquals('EventStubNoName', $event->getName());
   }
 }
