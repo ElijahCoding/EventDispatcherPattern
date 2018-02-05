0165c572f4a402db8e185845ed1f67c10793f375
Elijah <elijahsharper1@gmail.com>
2018-02-05 13:55:02 +0300

    Fetching listeners and Refactorying

--- app/Core/Events/Dispatcher.php
+++ app/Core/Events/Dispatcher.php
@@ -13,8 +13,23 @@ class Dispatcher
     return $this->listeners;
   }
 
+  public function getListenersByEvent($name)
+  {
+    if (!$this->hasListeners($name)) {
+      return [];
+    }
+
+    return $this->listeners[$name];
+  }
+
+  public function hasListeners($name)
+  {
+      return isset($this->listeners[$name]);
+  }
+
   public function addListener($name, Listener $listener)
   {
     $this->listeners[$name][] = $listener;
   }
+
 }
--- tests/Unit/DispatcherTest.php
+++ tests/Unit/DispatcherTest.php
@@ -24,6 +24,34 @@ class DispatcherTest extends TestCase
 
     $dispatcher->addListener('UserSignedUp', new ListenerStub());
 
+    // var_dump($dispatcher->getListeners());
+
     $this->assertCount(1, $dispatcher->getListeners()['UserSignedUp']);
+  }
+
+  /** @test */
+  public function it_can_get_listeners_by_event_name()
+  {
+    $dispatcher = new Dispatcher();
+
+    $dispatcher->addListener('UserSignedUp', new ListenerStub());
+
+    $this->assertCount(1, $dispatcher->getListenersByEvent('UserSignedUp'));
+  }
+
+  /** @test */
+  public function it_returns_empty_array_if_no_listeners_set_for_events()
+  {
+    $dispatcher = new Dispatcher();
+
+    $this->assertCount(0, $dispatcher->getListenersByEvent('UserSignedUp'));
+  }
+
+  /** @test */
+  public function it_can_check_if_has_listener_registered_for_event()
+  {
+    $dispatcher = new Dispatcher();
+
+    $this->assertFalse($dispatcher->hasListeners('UserSignedUp'));
   }
 }
